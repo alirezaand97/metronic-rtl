aafdc656f3e9e978a6c99082d888d6f8f378adb8
Alireza Alinia <a.alinia@asax.ir>
2023-08-27 12:10:50 +0330

    Clear

--- src/app/_metronic/layout/layout.module.ts
+++ src/app/_metronic/layout/layout.module.ts
@@ -1,46 +1,39 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { InlineSVGModule } from 'ng-inline-svg-2';
-import { RouterModule, Routes } from '@angular/router';
+import { DrawersModule, DropdownMenusModule, ModalsModule } from '../partials';
 import {
   NgbDropdownModule,
   NgbProgressbarModule,
   NgbTooltipModule,
 } from '@ng-bootstrap/ng-bootstrap';
-import { TranslateModule } from '@ngx-translate/core';
-import { TranslationModule } from '../../modules/i18n';
-import { LayoutComponent } from './layout.component';
-import { ExtrasModule } from '../partials/layout/extras/extras.module';
-import { Routing } from '../../pages/routing';
-import { AsideComponent } from './components/aside/aside.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ContentComponent } from './components/content/content.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ScriptsInitComponent } from './components/scripts-init/scripts-init.component';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
-import { AsideMenuComponent } from './components/aside/aside-menu/aside-menu.component';
-import { TopbarComponent } from './components/topbar/topbar.component';
-import { PageTitleComponent } from './components/header/page-title/page-title.component';
-import { HeaderMenuComponent } from './components/header/header-menu/header-menu.component';
-import {
-  DrawersModule,
-  DropdownMenusModule,
-  ModalsModule,
-  EngagesModule,
-} from '../partials';
-import { EngagesComponent } from '../partials/layout/engages/engages.component';
-import { ThemeModeModule } from '../partials/layout/theme-mode-switcher/theme-mode.module';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SidebarLogoComponent } from './components/sidebar/sidebar-logo/sidebar-logo.component';
-import { SidebarMenuComponent } from './components/sidebar/sidebar-menu/sidebar-menu.component';
-import { SidebarFooterComponent } from './components/sidebar/sidebar-footer/sidebar-footer.component';
-import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AccountingComponent } from './components/toolbar/accounting/accounting.component';
 import { ClassicComponent } from './components/toolbar/classic/classic.component';
+import { CommonModule } from '@angular/common';
+import { ContentComponent } from './components/content/content.component';
 import { ExtendedComponent } from './components/toolbar/extended/extended.component';
+import { ExtrasModule } from '../partials/layout/extras/extras.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderMenuComponent } from './components/header/header-menu/header-menu.component';
+import { InlineSVGModule } from 'ng-inline-svg-2';
+import { LayoutComponent } from './layout.component';
+import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { PageTitleComponent } from './components/header/page-title/page-title.component';
 import { ReportsComponent } from './components/toolbar/reports/reports.component';
+import { Routing } from '../../pages/routing';
 import { SaasComponent } from './components/toolbar/saas/saas.component';
-import {SharedModule} from "../shared/shared.module";
+import { ScriptsInitComponent } from './components/scripts-init/scripts-init.component';
+import { SharedModule } from '../shared/shared.module';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { SidebarFooterComponent } from './components/sidebar/sidebar-footer/sidebar-footer.component';
+import { SidebarLogoComponent } from './components/sidebar/sidebar-logo/sidebar-logo.component';
+import { SidebarMenuComponent } from './components/sidebar/sidebar-menu/sidebar-menu.component';
+import { ThemeModeModule } from '../partials/layout/theme-mode-switcher/theme-mode.module';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationModule } from '../../modules/i18n';
 
 const routes: Routes = [
   {
@@ -53,17 +46,14 @@ const routes: Routes = [
 @NgModule({
   declarations: [
     LayoutComponent,
-    AsideComponent,
     HeaderComponent,
     ContentComponent,
     FooterComponent,
     ScriptsInitComponent,
     ToolbarComponent,
-    AsideMenuComponent,
     TopbarComponent,
     PageTitleComponent,
     HeaderMenuComponent,
-    EngagesComponent,
     SidebarComponent,
     SidebarLogoComponent,
     SidebarMenuComponent,
@@ -85,12 +75,11 @@ const routes: Routes = [
     ExtrasModule,
     ModalsModule,
     DrawersModule,
-    EngagesModule,
     DropdownMenusModule,
     NgbTooltipModule,
     TranslateModule,
     ThemeModeModule,
-    SharedModule
+    SharedModule,
   ],
   exports: [RouterModule],
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,21 @@
-import { NgModule, APP_INITIALIZER } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { BrowserModule } from '@angular/platform-browser';
 import { ClipboardModule } from 'ngx-clipboard';
-import { TranslateModule } from '@ngx-translate/core';
+import { FakeAPIService } from './_fake/fake-api.service';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http';
 import { InlineSVGModule } from 'ng-inline-svg-2';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AuthService } from './modules/auth/services/auth.service';
+import { TranslateModule } from '@ngx-translate/core';
 import { environment } from 'src/environments/environment';
-// #fake-start#
-import { FakeAPIService } from './_fake/fake-api.service';
-// #fake-end#
 
-function appInitializer(authService: AuthService) {
-  return () => {
-    return new Promise((resolve) => {
-      //@ts-ignore
-      authService.getUserByToken().subscribe().add(resolve);
-    });
-  };
-}
+// #fake-start#
+
+// #fake-end#
 
 @NgModule({
   declarations: [AppComponent],
@@ -44,14 +37,7 @@ function appInitializer(authService: AuthService) {
     InlineSVGModule.forRoot(),
     NgbModule,
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: appInitializer,
-      multi: true,
-      deps: [AuthService],
-    },
-  ],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
